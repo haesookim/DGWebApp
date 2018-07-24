53200cf6c1a9cc158bced0acbb9f7d45c6f55241
Heather Kim <haesoo1108@gmail.com>
2018-07-24 16:03:57 +0900

    Fixing but with bugs

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'blog/post_list.html', {})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -17,7 +17,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,4 +36,13 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
 # Create your views here.
